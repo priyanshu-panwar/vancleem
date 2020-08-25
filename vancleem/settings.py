bc88037763e7ccce79f5adf1c46c4e27057ffb19
Priyanshu Panwar <priyanshu009ch@gmail.com>
2020-08-25 13:37:44 +0530

    done

--- vancleem/settings.py
+++ vancleem/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,5 +161,3 @@ EMAIL_HOST = "smtp.gmail.com"
 ADMIN_MAIL_1 = "grantvancleem@gmail.com"
 ADMIN_MAIL_2 = "priyanshu009ch@gmail.com"
 
-
-django_heroku.settings(locals())
